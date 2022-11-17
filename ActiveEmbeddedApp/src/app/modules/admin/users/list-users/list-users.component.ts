b527fea25188127876ceec0c800b65287329a4cb
lucas-de-F <francalucas937@gmail.com>
2022-11-17 16:58:08 -0300

    feat: add paginator a lista de usuários

--- ActiveEmbeddedApp/src/app/modules/admin/users/list-users/list-users.component.ts
+++ ActiveEmbeddedApp/src/app/modules/admin/users/list-users/list-users.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UsuariosService } from 'app/modules/services/usuarios';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface PeriodicElement {
   matricula: string;
@@ -15,31 +17,50 @@ export interface PeriodicElement {
   templateUrl: './list-users.component.html',
   styleUrls: ['./list-users.component.scss'],
 })
-export class ListUsersComponent implements OnInit {
+export class ListUsersComponent implements AfterViewInit, OnInit {
+  @ViewChild('paginator') paginator: MatPaginator;
+
   displayedColumns: string[] = ['matricula', 'nome', 'perfil', 'area', 'opcoes'];
-  dataSource = [];
+  usuarios: MatTableDataSource<PeriodicElement>
+  usuariosL: number = 0
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private usuariosService: UsuariosService
   ) {
-    this.dataSource = this.usuariosService.getUsuario()
   }
 
-  ngOnInit(): void { }
+  ngAfterViewInit(): void {
+    this.requisicoes()
+  }
+
+  ngOnInit(): void {
+    this.requisicoes()
+  }
+  requisicoes() {
+    const users = this.usuariosService.getUsuario()
+    this.usuarios = new MatTableDataSource(users);
+    this.usuarios.paginator = this.paginator;
+    this.usuariosL = this.usuarios?.data.length;
+  }
   deletarUsuario(matricula): void {
     this.usuariosService.deleteUsuario(matricula)
+    this.requisicoes()
   }
   criarUsuario(): void {
     this.router.navigate(['../usuarios-criar'], {
       relativeTo: this.route,
     })
+    this.requisicoes()
+
   }
   editarUsuario(matricula): void {
     this.router.navigate(['../usuarios-editar'], {
       relativeTo: this.route,
       queryParams: { matricula }
     });
+    this.requisicoes()
+
   }
 }
