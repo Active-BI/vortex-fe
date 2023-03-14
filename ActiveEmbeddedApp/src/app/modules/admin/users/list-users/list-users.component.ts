cfeb4a7ccecbce10220f7493af827fcea9f64c8c
lucas-de-F <francalucas937@gmail.com>
2023-03-14 11:02:43 -0300

    fix: ajuste de visualização de role do usuário

--- ActiveEmbeddedApp/src/app/modules/admin/users/list-users/list-users.component.ts
+++ ActiveEmbeddedApp/src/app/modules/admin/users/list-users/list-users.component.ts
@@ -1,91 +1,110 @@
-import { AfterViewInit, Component, Inject, OnInit, ViewChild } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    Inject,
+    OnInit,
+    ViewChild,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteModalComponent } from '../../delete-modal/delete-modal.component';
 import { ToastrService } from 'ngx-toastr';
-import { AdminService, getAllRequest } from 'app/modules/services/admin.service';
+import {
+    AdminService,
+    getAllRequest,
+} from 'app/modules/services/admin.service';
 import { FormControl } from '@angular/forms';
 import { DatePipe } from '@angular/common';
+import { listRoles } from '../usersUtils';
 
 export interface PeriodicElement {
-  id: string;
-  email: string;
-  emailContato: string;
-  identificacao: string;
-  nome: string;
-  perfil: string;
-  visoes: any[]
-  menus: any[string]
+    id: string;
+    email: string;
+    emailContato: string;
+    identificacao: string;
+    nome: string;
+    perfil: string;
+    visoes: any[];
+    menus: any[string];
 }
 
-
-
 @Component({
-  selector: 'app-list-users',
-  templateUrl: './list-users.component.html',
-  styleUrls: ['./list-users.component.scss'],
+    selector: 'app-list-users',
+    templateUrl: './list-users.component.html',
+    styleUrls: ['./list-users.component.scss'],
 })
 export class ListUsersComponent implements AfterViewInit, OnInit {
-  @ViewChild('paginator') paginator: MatPaginator;
-  myControl = new FormControl('');
-  pipe = new DatePipe('en-US');
+    @ViewChild('paginator') paginator: MatPaginator;
+    myControl = new FormControl('');
+    pipe = new DatePipe('en-US');
 
-  displayedColumns: string[] = ['nome', 'identificacao', 'perfil', 'ultimoAcesso', 'opcoes'];
-  usuarios: MatTableDataSource<getAllRequest>
-  usuariosL: number = 0
-  constructor(
-    private router: Router,
-    private adminSrv: AdminService,
-    public dialog: MatDialog,
-    private toastr: ToastrService,
-  ) {
-  }
+    displayedColumns: string[] = [
+        'nome',
+        'identificacao',
+        'perfil',
+        'ultimoAcesso',
+        'opcoes',
+    ];
+    usuarios: MatTableDataSource<getAllRequest>;
+    usuariosL: number = 0;
+    constructor(
+        private router: Router,
+        private adminSrv: AdminService,
+        public dialog: MatDialog,
+        private toastr: ToastrService
+    ) {}
 
-  ngAfterViewInit(): void {
-    this.requisicoes()
-  }
+    ngAfterViewInit(): void {
+        this.requisicoes();
+    }
 
-  ngOnInit(): void {
-    this.requisicoes()
-  }
-  requisicoes() {
-    this.adminSrv.getUsers().subscribe(e => {
-      this.usuarios = new MatTableDataSource(e);
-      this.usuariosFiltrados = new MatTableDataSource(e);
-      this.usuariosFiltrados.paginator = this.paginator;
-      this.usuarios.paginator = this.paginator;
-      this.usuariosL = this.usuarios?.data.length;
-    } )
+    ngOnInit(): void {
+        this.requisicoes();
+    }
+    requisicoes() {
+        this.adminSrv.getUsers().subscribe((e) => {
+            e = e.map((user) => ({
+                ...user,
+                perfil: listRoles.find((perfil) => user.perfilId === perfil.id)
+                    .name,
+            }));
 
-  }
-  usuariosFiltrados: MatTableDataSource<getAllRequest>
-  filtarUsuarios(e) {
-    const data = this.usuarios.data.filter((u) => u.nome.toUpperCase().includes(e.toUpperCase()))
-    this.usuariosFiltrados = new MatTableDataSource(data)
-    this.usuariosFiltrados.paginator = this.paginator;
-  }
-  deletarUsuario(id): void {
-    this.dialog.open(DeleteModalComponent, {
-      data: { 
-        nome: "Usuários",
-        data: () => {
-          this.dialog.closeAll()
-        this.adminSrv.deleteUser(id).subscribe(() => {
-          this.toastr.success("Deletado com Sucesso")
-          this.requisicoes()
-        })
-        }
-      },
-  });
-  }
+            this.usuarios = new MatTableDataSource(e);
+            this.usuariosFiltrados = new MatTableDataSource(e);
+            this.usuariosFiltrados.paginator = this.paginator;
+            this.usuarios.paginator = this.paginator;
+            this.usuariosL = this.usuarios?.data.length;
+        });
+    }
+    usuariosFiltrados: MatTableDataSource<getAllRequest>;
+    filtarUsuarios(e) {
+        const data = this.usuarios.data.filter((u) =>
+            u.nome.toUpperCase().includes(e.toUpperCase())
+        );
+        this.usuariosFiltrados = new MatTableDataSource(data);
+        this.usuariosFiltrados.paginator = this.paginator;
+    }
+    deletarUsuario(id): void {
+        this.dialog.open(DeleteModalComponent, {
+            data: {
+                nome: 'Usuários',
+                data: () => {
+                    this.dialog.closeAll();
+                    this.adminSrv.deleteUser(id).subscribe(() => {
+                        this.toastr.success('Deletado com Sucesso');
+                        this.requisicoes();
+                    });
+                },
+            },
+        });
+    }
 
-  criarUsuario(): void {
-    this.router.navigate(['app/usuarios-criar'])
-
-  }
-  editarUsuario(id): void {
-    this.router.navigate([`app/usuarios-editar/${id}`])
-  }
+    criarUsuario(): void {
+        this.router.navigate(['app/usuarios-criar']);
+    }
+    editarUsuario(id): void {
+        this.router.navigate([`app/usuarios-editar/${id}`]);
+    }
 }
--- ActiveEmbeddedApp/src/app/modules/services/admin.service.ts
+++ ActiveEmbeddedApp/src/app/modules/services/admin.service.ts
@@ -7,99 +7,110 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, debounceTime, map } from 'rxjs/operators';
 
 export interface getAllRequest {
-  id: string;
-  email: string;
-  emailContato: string;
-  identificacao: string;
-  nome: string;
-  perfil: string;
-  userVisions: any[] | null
-  dataUltimoAcesso: string | null
+    id: string;
+    email: string;
+    emailContato: string;
+    identificacao: string;
+    nome: string;
+    perfilId: string;
+    userVisions: any[] | null;
+    dataUltimoAcesso: string | null;
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
-
-
 export class AdminService {
+    constructor(private http: HttpClient, private toast: ToastrService) {}
 
-  constructor(private http: HttpClient,
-              private toast: ToastrService ) { }
+    private baseUrl = environment.baseUrl;
 
- private baseUrl = environment.baseUrl;
+    getUsers(): Observable<getAllRequest[]> {
+        return this.http.get<any[]>(`${this.baseUrl}admin`).pipe(
+            catchError((err) => {
+                ('');
+                this.toast.error(`Erro ao consultar usuários`, null, {
+                    progressBar: true,
+                    timeOut: 2000,
+                });
+                return throwError(err);
+            })
+        );
+    }
 
-  getUsers(): Observable<getAllRequest[]>{
-    return this.http.get<any[]>(`${this.baseUrl}admin`)
-    .pipe(
-      catchError((err) => {''
-        this.toast.error(`Erro ao consultar usuários`,null,{progressBar:true, timeOut:2000});
-        return throwError(err);
-      })
-    )
-  }
+    getVisions(): Observable<any[]> {
+        return this.http.get<any[]>(`${this.baseUrl}visoes`).pipe(
+            map((visions: any[]) => {
+                return visions.sort((a, b) => {
+                    a = a.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+                    b = b.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+                    if (a > b) {
+                        return 1;
+                    }
+                    if (a < b) {
+                        return -1;
+                    }
+                    // a must be equal to b
+                    return 0;
+                });
+            }),
+            catchError((err) => {
+                this.toast.error(`Erro ao obter visões`, null, {
+                    progressBar: true,
+                    timeOut: 2000,
+                });
+                return throwError(err);
+            })
+        );
+    }
 
-  getVisions(): Observable<any[]>{
-    return this.http.get<any[]>(`${this.baseUrl}visoes`)
-    .pipe(
-      map((visions: any[]) => {
-        return visions.sort((a, b) => {
-          a = a.name.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-          b = b.name.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-          if (a > b) {
-            return 1;
-          }
-          if (a < b) {
-            return -1;
-          }
-          // a must be equal to b
-          return 0;
-        })
-      } ),
-      catchError((err) => {
-        this.toast.error(`Erro ao obter visões`,null,{progressBar:true, timeOut:2000});
-        return throwError(err);
-      })
-    )
-  }
+    getUserById(userId: string): Observable<any> {
+        return this.http.get<any>(`${this.baseUrl}admin/${userId}`).pipe(
+            catchError((err) => {
+                this.toast.error(`Erro ao consultar usuário`, null, {
+                    progressBar: true,
+                    timeOut: 2000,
+                });
+                return throwError(err);
+            })
+        );
+    }
 
-  getUserById(userId: string): Observable<any>{
-    return this.http.get<any>(`${this.baseUrl}admin/${userId}`)
-    .pipe(
-      catchError((err) => {
-        this.toast.error(`Erro ao consultar usuário`,null,{progressBar:true, timeOut:2000});
-        return throwError(err);
-      })
-    )
-  }
+    updateUser(user: any): Observable<any> {
+        return this.http.put<any>(`${this.baseUrl}admin/user`, user).pipe(
+            catchError((err) => {
+                this.toast.error(`Erro ao atualizar usuários`, null, {
+                    progressBar: true,
+                    timeOut: 2000,
+                });
+                return throwError(err);
+            })
+        );
+    }
 
-  updateUser(user:any): Observable<any>{
-    return this.http.put<any>(`${this.baseUrl}admin/user`,user)
-    .pipe(
-      catchError((err) => {
-        this.toast.error(`Erro ao atualizar usuários`,null,{progressBar:true, timeOut:2000});
-        return throwError(err);
-      })
-    )
-  }
+    preRegister(user: any): Observable<any> {
+        return this.http.post<any>(`${this.baseUrl}admin`, user).pipe(
+            catchError((err) => {
+                this.toast.error(`Erro ao pré-cadastrar usuário`, null, {
+                    progressBar: true,
+                    timeOut: 2000,
+                });
+                return throwError(err);
+            })
+        );
+    }
 
-  preRegister(user:any): Observable<any>{
-    return this.http.post<any>(`${this.baseUrl}admin`,user)
-    .pipe(
-      catchError((err) => {
-        this.toast.error(`Erro ao pré-cadastrar usuário`,null,{progressBar:true, timeOut:2000});
-        return throwError(err);
-      })
-    )
-  }
-
-  deleteUser(userId: string): Observable<any>{
-    return this.http.delete<any>(`${this.baseUrl}admin?userId=${userId}`)
-    .pipe(
-      catchError((err) => {
-        this.toast.error(`Erro ao remover usuário`,null,{progressBar:true, timeOut:2000});
-        return throwError(err);
-      })
-    )
-  }
+    deleteUser(userId: string): Observable<any> {
+        return this.http
+            .delete<any>(`${this.baseUrl}admin?userId=${userId}`)
+            .pipe(
+                catchError((err) => {
+                    this.toast.error(`Erro ao remover usuário`, null, {
+                        progressBar: true,
+                        timeOut: 2000,
+                    });
+                    return throwError(err);
+                })
+            );
+    }
 }
