2ded681d21cd934a2f5b0b69611060a1f009e12f
lucas-de-F <francalucas937@gmail.com>
2023-03-16 16:09:37 -0300

    feat: ajustando build

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static('dist/fuse'))
+app.use(express.static('dist'))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../dist/fuse', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '../dist', 'index.html'))
 })
 
 app.listen(port, () => {
