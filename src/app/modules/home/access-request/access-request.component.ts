5ee49342fe9b0041ef583f02b3077d4bd9f8cfad
lucas-de-F <francalucas937@gmail.com>
2023-08-09 11:40:08 -0300

    feat: melhorando validação de solicitação

--- src/app/modules/home/access-request/access-request.component.ts
+++ src/app/modules/home/access-request/access-request.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { AdminRequestService } from 'app/modules/services/admin-request.service';
 import { ToastrService } from 'ngx-toastr';
@@ -14,16 +15,39 @@ export class AccessRequestComponent implements OnInit {
     setTenat = false;
     atualizarValor() {
         this.setTenat = !this.setTenat;
+
+        if (this.setTenat) {
+            this.form = this._formBuilder.group({
+                email: [this.form.value.email, [Validators.required]],
+                name: [this.form.value.name, [Validators.required]],
+                description: [this.form.value.description],
+                profession: [this.form.value.profession, [Validators.required]],
+                company_name: [
+                    this.form.value.company_name,
+                    [Validators.required],
+                ],
+                company_cnpj: [
+                    this.form.value.company_cnpj,
+                    [Validators.required],
+                ],
+                company_description: [
+                    this.form.value.company_description,
+                    [Validators.required],
+                ],
+            });
+        } else {
+            this.form = this._formBuilder.group({
+                email: [this.form.value.email, [Validators.required]],
+                name: [this.form.value.name, [Validators.required]],
+                description: [this.form.value.description],
+                profession: [this.form.value.profession, [Validators.required]],
+                company_name: [this.form.value.company_name],
+                company_cnpj: [this.form.value.company_cnpj],
+                company_description: [this.form.value.company_description],
+            });
+        }
     }
-    form = this._formBuilder.group({
-        email: ['', [Validators.required]],
-        name: ['', [Validators.required]],
-        description: [''],
-        profession: ['', [Validators.required]],
-        company_name: [''],
-        company_cnpj: [''],
-        company_description: [''],
-    });
+    form;
     checkScreenSize() {
         return window.innerWidth >= 768;
     }
@@ -31,14 +55,21 @@ export class AccessRequestComponent implements OnInit {
         private _formBuilder: FormBuilder,
         private toastr: ToastrService,
         private router: Router,
-        private adminRequestService: AdminRequestService
-    ) {}
+        private adminRequestService: AdminRequestService,
+        public dialog: MatDialog
+    ) {
+        this.form = this._formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            name: ['', [Validators.required]],
+            description: [''],
+            profession: ['', [Validators.required]],
+            company_name: [''],
+            company_cnpj: [''],
+            company_description: [''],
+        });
+    }
 
     submit() {
-        const validator = this.setTenat === true ? [Validators.required] : [];
-        this.form.get('company_name').setValidators(validator);
-        this.form.get('company_cnpj').setValidators(validator);
-        this.form.get('company_description').setValidators(validator);
         if (!this.form.valid) {
             this.toastr.error('Campos inválidos');
             return;
