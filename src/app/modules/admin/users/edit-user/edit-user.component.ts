75fa519384056a861913f72a721ad006c7ebadc7
lucas-de-F <francalucas937@gmail.com>
2023-08-10 17:14:08 -0300

    feat: ajuste de select de visoes de pagina

--- src/app/modules/admin/users/edit-user/edit-user.component.ts
+++ src/app/modules/admin/users/edit-user/edit-user.component.ts
@@ -5,9 +5,7 @@ import { AdminService } from 'app/modules/services/admin.service';
 import { listRoles } from 'app/modules/services/roles.service';
 import { ToastrService } from 'ngx-toastr';
 import { ordersData } from '../usersUtils';
-import { PMIService } from 'app/modules/services/PMI.service';
 import { DashboardService } from 'app/modules/services/dashboard.service';
-import jwtDecode from 'jwt-decode';
 
 @Component({
     selector: 'app-edit-user',
@@ -18,6 +16,7 @@ export class EditUserComponent implements OnInit {
     myControl = new FormControl('');
     tenants: string[] = [];
     selectedTenant = this.tenants;
+    visoes = new FormControl([]);
 
     onKey(value) {
         this.selectedTenant = this.search(value);
@@ -53,6 +52,7 @@ export class EditUserComponent implements OnInit {
     dashboardList = [];
     selectedDashboardList = [];
 
+    dashboardListReduced = [];
     listRoles = listRoles;
     constructor(
         private fb: FormBuilder,
@@ -76,19 +76,38 @@ export class EditUserComponent implements OnInit {
                     selected: false,
                 };
             });
+            this.dashboardListReduced = this.dashboardList.reduce(
+                (acc, cur) => {
+                    const findItem = acc.findIndex(
+                        (a) => a.page_group === cur.page_group
+                    );
+                    if (findItem >= 0) {
+                        acc[findItem].children.push(cur);
+                        return acc;
+                    }
+                    acc.push({
+                        group_name: cur.page_group,
+                        children: [cur],
+                    });
+                    return acc;
+                },
+                []
+            );
+
             if (editar) {
                 this.adminSrv.getUserById(this.id).subscribe((e: any) => {
                     this.user = e;
-                    let tenant;
                     this.selectedDashboardList = this.dashboardList.map(
-                        (dash) => ({
-                            ...dash,
-                            selected: this.user.User_Page.find(
-                                (utd) => utd.tenant_page_id === dash.id
-                            )
-                                ? true
-                                : false,
-                        })
+                        (dash) => {
+                            this.user.User_Page.find((utd) => {
+                                if (utd.tenant_page_id === dash.id) {
+                                    this.visoes.setValue([
+                                        ...this.visoes.value,
+                                        dash.id,
+                                    ]);
+                                }
+                            });
+                        }
                     );
 
                     this.form.patchValue({
@@ -128,9 +147,7 @@ export class EditUserComponent implements OnInit {
             this.dashboardService
                 .postDashboards(
                     {
-                        DashboardUserList: this.selectedDashboardList
-                            .filter((e) => e.selected)
-                            .map((e) => e.id),
+                        DashboardUserList: this.visoes.value,
                     },
                     this.id
                 )
@@ -148,175 +165,3 @@ export class EditUserComponent implements OnInit {
         }
     }
 }
-
-// import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, Validators, FormControl } from '@angular/forms';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { AdminService } from 'app/modules/services/admin.service';
-// import { listRoles } from 'app/modules/services/roles.service';
-// import { ToastrService } from 'ngx-toastr';
-// import { ordersData } from '../usersUtils';
-// import { DashboardService } from 'app/modules/services/dashboard.service';
-
-// @Component({
-//     selector: 'app-edit-user',
-//     templateUrl: './edit-user.component.html',
-//     styleUrls: ['./edit-user.component.scss'],
-// })
-// export class EditUserComponent implements OnInit {
-//     myControl = new FormControl('');
-//     tenants: string[] = [];
-//     selectedTenant = this.tenants;
-//     visoes = new FormControl([]);
-
-//     onKey(value) {
-//         this.selectedTenant = this.search(value);
-//     }
-
-//     search(value: string) {
-//         let filter = value.toLowerCase();
-//         return this.tenants.filter((option: any) =>
-//             option.nome_cliente.toLowerCase().startsWith(filter)
-//         );
-//     }
-
-//     visionsSelecteds = [];
-//     form = this.fb.group({
-//         id: [''],
-//         name: ['', [Validators.required, Validators.minLength(3)]],
-//         email: [
-//             '',
-//             [
-//                 Validators.required,
-//                 Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
-//                 Validators.email,
-//             ],
-//         ],
-//         profession: ['', [Validators.required]],
-//         description: ['', [Validators.required]],
-//         rls_id: ['', [Validators.required]],
-//     });
-//     panelOpenState = false;
-//     id: string;
-//     ordersData = ordersData;
-//     user: any;
-//     dashboardList = [];
-//     selectedDashboardList = [];
-
-//     dashboardListReduced = [];
-//     listRoles = listRoles;
-//     constructor(
-//         private fb: FormBuilder,
-//         private router: Router,
-//         private route: ActivatedRoute,
-//         private toastr: ToastrService,
-//         private adminSrv: AdminService,
-//         private dashboardService: DashboardService
-//     ) {
-//         this.id = this.route.snapshot.paramMap.get('id');
-//         let editar = false;
-//         this.route.url.subscribe(
-//             (a) => (editar = a[0].path.includes('editar'))
-//         );
-//         this.dashboardService.getDashboards().subscribe((e: any[]) => {
-//             this.dashboardList = e.map((tenant_dashboard) => {
-//                 return {
-//                     page_group: tenant_dashboard.Page.Page_Group.title,
-//                     name: tenant_dashboard.Page.title,
-//                     id: tenant_dashboard.id,
-//                     selected: false,
-//                 };
-//             });
-//             this.dashboardListReduced = this.dashboardList.reduce(
-//                 (acc, cur) => {
-//                     const findItem = acc.findIndex(
-//                         (a) => a.page_group === cur.page_group
-//                     );
-//                     if (findItem >= 0) {
-//                         acc[findItem].children.push(cur);
-//                         return acc;
-//                     }
-//                     acc.push({
-//                         group_name: cur.page_group,
-//                         children: [cur],
-//                     });
-//                     return acc;
-//                 },
-//                 []
-//             );
-
-//             if (editar) {
-//                 this.adminSrv.getUserById(this.id).subscribe((e: any) => {
-//                     this.user = e;
-//                     let tenant;
-//                     this.selectedDashboardList = this.dashboardList.map(
-//                         (dash) => {
-//                             this.user.User_Page.find((utd) => {
-//                                 if (utd.tenant_page_id === dash.id) {
-//                                     console.log(dash);
-//                                     this.visoes.setValue([
-//                                         // ...this.visoes.value,
-//                                         // dash.id,
-//                                     ]);
-//                                 }
-//                             });
-//                         }
-//                     );
-
-//                     this.form.patchValue({
-//                         id: this.user.id,
-//                         name: this.user.name,
-//                         email: this.user.contact_email,
-//                         rls_id: this.user.rls_id,
-//                         profession: this.user.profession,
-//                         description: this.user.description,
-//                     });
-//                 });
-//             }
-//         });
-//     }
-
-//     ngOnInit(): void {
-//         this.form.controls.email.disable();
-//     }
-
-//     filteredVisions = [];
-//     checkedDash(item, index) {
-//         this.selectedDashboardList[index] = {
-//             ...item,
-//             selected: !item.selected,
-//         };
-//     }
-//     voltar(): void {
-//         this.router.navigate(['app/usuarios']);
-//     }
-
-//     redirectToEdit(id) {
-//         this.router.navigate([`app/usuarios-editar/${id}`]);
-//     }
-
-//     editar(): void {
-//         if (this.form.valid && this.myControl.valid) {
-//             this.dashboardService
-//                 .postDashboards(
-//                     {
-//                         DashboardUserList: this.selectedDashboardList
-//                             .filter((e) => e.selected)
-//                             .map((e) => e.id),
-//                     },
-//                     this.id
-//                 )
-//                 .subscribe((e) => {});
-//             this.adminSrv
-//                 .updateUser({
-//                     ...this.form.value,
-//                 })
-//                 .subscribe((e) => {
-//                     this.toastr.success('Editado com Sucesso');
-//                     this.voltar();
-//                 });
-//         } else {
-//             this.form.markAllAsTouched();
-//         }
-//     }
-// }
