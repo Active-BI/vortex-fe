19a72143d96f79c2f1c2417c5cf0e2bf39895dcc
DAmalek <danielmalek101@gmail.com>
2024-03-04 15:02:51 -0300

    merge com a branch main

--- src/app/modules/master/tenant/configs/configs.component.ts
+++ src/app/modules/master/tenant/configs/configs.component.ts
@@ -1,80 +1,85 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { AdminService } from 'app/modules/services/admin.service';
+import { UserService } from 'app/modules/services/user.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-configs',
-  templateUrl: './configs.component.html',
-  styleUrls: ['./configs.component.scss']
+    selector: 'app-configs',
+    templateUrl: './configs.component.html',
+    styleUrls: ['./configs.component.scss'],
 })
 export class ConfigsComponent implements OnInit {
+    constructor(
+        private fb: FormBuilder,
+        private userSrv: UserService,
+        private toastr: ToastrService
+    ) {}
 
-  constructor(private fb: FormBuilder, private adminService: AdminService, private toastr: ToastrService) { }
+    ngOnInit(): void {
+        this.userSrv.obterConfiguracoesDePagina().subscribe((res) => {
+            this.form.patchValue({
+                ...res,
+            });
+        });
+    }
+    form = this.fb.group({
+        logo: ['', [Validators.required]],
+        bg_image: ['', [Validators.required]],
+        bg_color: ['', [Validators.required]],
+    });
 
-  ngOnInit(): void {
-    this.adminService.obterConfiguracoesDePagina().subscribe(res => {
-      this.form.patchValue({
-        ...res
-      })
-    })
-  }
-  form = this.fb.group({
-    logo: ['', [Validators.required]],
-    bg_image: ['', [Validators.required]],
-    bg_color: ['', [Validators.required]],
-  })
+    onColorSelected(color: string, label) {
+        this.form.patchValue({
+            [label]: color,
+        });
+    }
+    clearLogoFileSelection() {
+        this.form.patchValue({
+            logo: '',
+        });
+    }
+    clearBgImageFileSelection() {
+        this.form.patchValue({
+            bg_image: '',
+        });
+    }
+    onFileLogoSelected(event: any) {
+        const file = event.target.files[0];
 
-  
-  onColorSelected(color: string,label) {
-    this.form.patchValue({
-      [label]: color
-    })
-  }
-  clearLogoFileSelection() {
-      this.form.patchValue({
-        logo: ''
-      })
-  }
-  clearBgImageFileSelection() {
-      this.form.patchValue({
-        bg_image: ''
-      })
-  }
-  onFileLogoSelected(event: any) {
-      const file = event.target.files[0];
-      
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = () => {
-          this.form.patchValue({
-              logo:  reader.result as string
-          })
-        };
-  
-        reader.readAsDataURL(file);
-      }
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                this.form.patchValue({
+                    logo: reader.result as string,
+                });
+            };
+
+            reader.readAsDataURL(file);
+        }
     }
     salvar() {
-      this.adminService.salvarConfiguracoesDePagina(this.form.value).subscribe(res => {
-        this.toastr.success('Configurações salvas com sucesso')
-      }, ({error}) => {
-        this.toastr.error('Falha ao atualizar configurações')
-      })
+        this.userSrv.salvarConfiguracoesDePagina(this.form.value).subscribe(
+            (res) => {
+                this.toastr.success('Configurações salvas com sucesso');
+            },
+            ({ error }) => {
+                this.toastr.error('Falha ao atualizar configurações');
+            }
+        );
     }
 
-    onBgImageFileSelected(event: any ) {
-      const file = event.target.files[0];
-      
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = () => {
-          this.form.patchValue({
-              bg_image:  reader.result as string
-          })
-        };
-  
-        reader.readAsDataURL(file);
-      }
+    onBgImageFileSelected(event: any) {
+        const file = event.target.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                this.form.patchValue({
+                    bg_image: reader.result as string,
+                });
+            };
+
+            reader.readAsDataURL(file);
+        }
     }
 }
