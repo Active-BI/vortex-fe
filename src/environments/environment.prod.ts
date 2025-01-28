5ea7b0216460d10efedc81616d202b2138c63ead
Daniel Abdel <danielmalek101@gmail.com>
2025-01-28 17:09:30 -0300

    fix: type error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: ' https://vortex-api-416840a7504f.herokuapp.com//api/',
+    baseUrl: ' https://vortex-api-416840a7504f.herokuapp.com/api/',
     socketUrl: ' https://vortex-api-416840a7504f.herokuapp.com/',
 };
